7435e1ff6ce7ade9b84276cb56e29f7b93099919
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-30 19:17:42 +0600

    hide user & password

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.xn4aldo.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xn4aldo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -15,12 +14,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Simple crud is running')
+})
+
+app.listen(port, () => {
+  console.log(`Simple crud is running on port ${port}`)
+})
+
