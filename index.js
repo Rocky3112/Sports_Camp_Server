b5ff4b3a51dee3e1a4a61b595c26af7aa8204e3e
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-30 19:24:16 +0600

    use necessary components & middle ware

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express =require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+const cors = require('cors');
+require('dotenv').config()
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xn4aldo.mongodb.net/?retryWrites=true&w=majority`;
 
