2cd0cd2631193ea941b2d468da64cf16c538674b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-08-01 01:00:58 +0600

    use verify JWT to secure

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 //middleware
 app.use(cors())
 app.use(express.json())
 
+//verify jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'access denied for unauthorized' });
+  }
+  
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'access denied for unauthorized' })
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xn4aldo.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +48,13 @@ async function run() {
     const instractorsCollection = client.db("sportsCamp").collection("instractors");
     const userCollection = client.db("sportsCamp").collection("users");
 
+    
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+
+      res.send({ token })
+    })
     app.get('/classes', async (req, res) => {
         const result = await classCollection.find().toArray();
         res.send(result);
@@ -56,7 +81,21 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
-      
+
+
+      app.get('/users/admin/:email',verifyJWT, async (req, res) => {
+        const email = req.params.email;
+ 
+        if (req.decoded.email !== email) {
+          res.send({ admin: false })
+        }
+  
+        const query = { email: email }
+        const user = await userCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' }
+        res.send(result);
+      })
+
       app.patch('/users/admin/:id', async (req, res) => {
         const userId = req.params.id;
         const filter = { _id: new ObjectId(userId) };
